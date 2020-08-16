6c7762ecd7ecbaa6565253bc899ba647b4c97d65
James Fisher <jfisher396@hotmail.com>
2020-08-16 15:21:16 -0700

    set up search functions

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,37 +1,78 @@
-import React, { Component } from "react"
-import axios from 'axios'
+import React, { Component } from "react";
+import API from "../utils/API";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+import Card from "../components/Card";
+import SearchForm from "../components/SearchForm";
+import BookDetail from "../components/BookDetail";
 
-
-  export default class Books extends Component {
+class Books extends Component {
   state = {
-    title: "",
-    authors: "",
-    description: "",
+    result: {},
+    search: "",
+  };
 
+  componentDidMount() {
+    this.searchBooks("The Hobbit");
   }
 
-  handleFormSubmit = (e) => {
-    e.preventDefault();
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + this.state.title).then(data => {
-      console.log(data.data.items[0].volumeInfo.authors);
-      this.setState({
-        title: "",
-        authors: "",
-        description: ""
-      })
-    })
+  searchBooks = (query) => {
+    API.search(query)
+    // console.log(response)
+      .then((res) => this.setState({ result: res.data }))
+      
+      .catch((err) => console.log(err));
   }
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.searchBooks(this.state.search);
+  }
+  // 
 
   render() {
     return (
-      <div>
-        <h1>Search Component</h1>
-        <form>
-          <input type="text" name="title" placeholder="title to search" value={this.state.title} onChange={(e) => this.setState({ title: e.target.value })} />
-          <button onClick={this.handleFormSubmit}>Search!</button>
-        </form>
-        <h1>{this.state.title}</h1>
-      </div>
-    )
+      <Container>
+        <Row>
+          <Col size="md-8">
+            <Card
+              heading={this.state.result.Title || "Search for a Book to Begin"}
+            >
+              {this.state.result.Title ? (
+                <BookDetail
+                  title={this.state.result.Title}
+                  src={this.state.result.Poster}
+                  director={this.state.result.Director}
+                  genre={this.state.result.Genre}
+                  released={this.state.result.Released}
+                />
+              ) : (
+                <h3>No Results to Display</h3>
+              )}
+            </Card>
+          </Col>
+          <Col size="md-4">
+            <Card heading="Search">
+              <SearchForm
+                value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
+
+export default Books;
